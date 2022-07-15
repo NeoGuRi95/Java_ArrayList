39cd833ecb51869b9cec9c23c62bb14f7d34dcc9
NeoGuRi95 <jea5158@gmail.com>
2022-07-15 10:54:35 +0900

    add 함수까지 구현

--- src/main/java/com/ll/exam/ArrayList.java
+++ src/main/java/com/ll/exam/ArrayList.java
@@ -1,5 +1,37 @@
 package com.ll.exam;
 
 public class ArrayList {
+    private int size;
+    private int[] arr;
 
+    public ArrayList() {
+        this.size = 0;
+        this.arr = new int[2];
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void add(int data) {
+        if (isFull()) {
+            expandSize();
+        }
+
+        this.arr[size++] = data;
+    }
+
+    public boolean isFull() {
+        return this.arr.length == this.size;
+    }
+
+    public void expandSize() {
+        int[] tempArr = new int[2 * this.size];
+
+        for (int i = 0; i < arr.length; i++) {
+            tempArr[i] = this.arr[i];
+        }
+
+        this.arr = tempArr;
+    }
 }
